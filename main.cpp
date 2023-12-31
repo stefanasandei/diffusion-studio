5383fc20e2dbf89cdf12872008f56cb9ad605b40
Asandei Stefan <asandei.stefanel@gmail.com>
2023-12-31 17:10:13 +0200

    add sd.cpp as a lib

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <print>
-
-int main() {
-    std::print("Hello, C++23!");
-    return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <print>
+
+#include <stdio.h>
+#include <ctime>
+#include <random>
+#include "ggml/ggml.h"
+#include "stable-diffusion.h"
+
+int main() {
+    StableDiffusion sd;
+    std::print("Hello, C++23!");
+    return 0;
+}
